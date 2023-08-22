ea055c338dd34def51a0d25560869b838aafa649
Hirotaka Aoki <113173839+aoki-h-jp@users.noreply.github.com>
2023-08-23 01:35:14 +0900

    Initial commit

---
+++ downloader/__init__.py
@@ -0,0 +1,4 @@
+"""
+BinanceBulkDownloader: A library to efficiently and concurrently download historical data from Binance.
+"""
+from .downloader import BinanceBulkDownloader
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='binance-bulk-downloader',
+    version="1.0.0",
+    description='A Python library to efficiently and concurrently download historical data files from Binance. Supports multiple asset types (spot, futures, options) and various data frequencies.',
+    install_requires=[],
+    author='aoki-h-jp',
+    author_email='aoki.hirotaka.biz@gmail.com',
+    license='MIT',
+    packages=find_packages(
+        include=['downloader'],
+        exclude=['tests']
+    ),
+)
