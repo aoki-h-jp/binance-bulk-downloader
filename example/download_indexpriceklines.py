3f4d441d34c74a1bdda8394aa674f403dd717bd0
Hirotaka Aoki <113173839+aoki-h-jp@users.noreply.github.com>
2023-08-25 18:39:38 +0900

    fix misspell

--- example/download_indexpriceklines.py
+++ example/download_indexpriceklines.py
@@ -2,46 +2,46 @@
 from downloader import BinanceBulkDownloader
 
 # generate instance
-downloader = BinanceBulkDownloader(data_type="indexpriceKlines")
+downloader = BinanceBulkDownloader(data_type="indexPriceKlines")
 
-# download indexpriceKlines (frequency: "1m", asset="um")
+# download indexPriceKlines (frequency: "1m", asset="um")
 downloader.run_download()
 
-# download indexpriceKlines (frequency: "5m", asset="um")
-downloader = BinanceBulkDownloader(data_type="indexpriceKlines", data_frequency="5m")
+# download indexPriceKlines (frequency: "5m", asset="um")
+downloader = BinanceBulkDownloader(data_type="indexPriceKlines", data_frequency="5m")
 downloader.run_download()
 
-# download indexpriceKlines (frequency: "1d", asset="um")
-downloader = BinanceBulkDownloader(data_type="indexpriceKlines", data_frequency="1d")
+# download indexPriceKlines (frequency: "1d", asset="um")
+downloader = BinanceBulkDownloader(data_type="indexPriceKlines", data_frequency="1d")
 downloader.run_download()
 
-# download monthly indexpriceKlines (frequency: "1m", asset="um", timeperiod_per_file="monthly")
+# download monthly indexPriceKlines (frequency: "1m", asset="um", timeperiod_per_file="monthly")
 downloader = BinanceBulkDownloader(
-    data_type="indexpriceKlines", data_frequency="1m", timeperiod_per_file="monthly"
+    data_type="indexPriceKlines", data_frequency="1m", timeperiod_per_file="monthly"
 )
 downloader.run_download()
 
-# download indexpriceKlines (frequency: "1m", asset="cm")
+# download indexPriceKlines (frequency: "1m", asset="cm")
 downloader = BinanceBulkDownloader(
-    data_type="indexpriceKlines", data_frequency="1m", asset="cm"
+    data_type="indexPriceKlines", data_frequency="1m", asset="cm"
 )
 downloader.run_download()
 
-# download indexpriceKlines (frequency: "5m", asset="cm")
+# download indexPriceKlines (frequency: "5m", asset="cm")
 downloader = BinanceBulkDownloader(
-    data_type="indexpriceKlines", data_frequency="5m", asset="cm"
+    data_type="indexPriceKlines", data_frequency="5m", asset="cm"
 )
 downloader.run_download()
 
-# download indexpriceKlines (frequency: "1d", asset="cm")
+# download indexPriceKlines (frequency: "1d", asset="cm")
 downloader = BinanceBulkDownloader(
-    data_type="indexpriceKlines", data_frequency="1d", asset="cm"
+    data_type="indexPriceKlines", data_frequency="1d", asset="cm"
 )
 downloader.run_download()
 
-# download monthly indexpriceKlines (frequency: "1m", asset="cm", timeperiod_per_file="monthly")
+# download monthly indexPriceKlines (frequency: "1m", asset="cm", timeperiod_per_file="monthly")
 downloader = BinanceBulkDownloader(
-    data_type="indexpriceKlines",
+    data_type="indexPriceKlines",
     data_frequency="1m",
     asset="cm",
     timeperiod_per_file="monthly",
