e8783cc724fe6085b8fff10a5453efe870949089
aoki-h-jp <113173839+aoki-h-jp@users.noreply.github.com>
2023-09-01 23:21:35 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="binance-bulk-downloader",
-    version="1.0.4",
+    version="1.0.5",
     description="A Python library to efficiently and concurrently download historical data files from Binance. Supports all asset types (spot, futures, options) and all frequencies.",
     install_requires=["requests", "rich", "pytest"],
     author="aoki-h-jp",
